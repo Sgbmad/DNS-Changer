e809a07046f8623cba14ee71b60986e0aa0de934
Sgbmad <sgbmad@gmail.com>
2024-11-12 14:04:18 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        cmbDNSSets = New ComboBox()
+        txtPrimaryDNS = New TextBox()
+        txtSecondaryDNS = New TextBox()
+        btnAdd = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnSet = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Icono = New NotifyIcon(components)
+        ctMenu = New ContextMenuStrip(components)
+        AbrirProgramaToolStripMenuItem = New ToolStripMenuItem()
+        CerrarToolStripMenuItem = New ToolStripMenuItem()
+        BorrarCachéToolStripMenuItem = New ToolStripMenuItem()
+        tmHide = New Timer(components)
+        ctMenu.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' cmbDNSSets
+        ' 
+        cmbDNSSets.FormattingEnabled = True
+        cmbDNSSets.Location = New Point(119, 19)
+        cmbDNSSets.Name = "cmbDNSSets"
+        cmbDNSSets.Size = New Size(136, 23)
+        cmbDNSSets.TabIndex = 0
+        ' 
+        ' txtPrimaryDNS
+        ' 
+        txtPrimaryDNS.Location = New Point(119, 48)
+        txtPrimaryDNS.Name = "txtPrimaryDNS"
+        txtPrimaryDNS.Size = New Size(136, 23)
+        txtPrimaryDNS.TabIndex = 1
+        ' 
+        ' txtSecondaryDNS
+        ' 
+        txtSecondaryDNS.Location = New Point(119, 76)
+        txtSecondaryDNS.Name = "txtSecondaryDNS"
+        txtSecondaryDNS.Size = New Size(136, 23)
+        txtSecondaryDNS.TabIndex = 2
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(268, 19)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(96, 23)
+        btnAdd.TabIndex = 3
+        btnAdd.Text = "AÑADIR"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(268, 48)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(96, 23)
+        btnUpdate.TabIndex = 4
+        btnUpdate.Text = "GUARDAR"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(268, 78)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(96, 23)
+        btnDelete.TabIndex = 5
+        btnDelete.Text = "BORRAR"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnSet
+        ' 
+        btnSet.Location = New Point(370, 19)
+        btnSet.Name = "btnSet"
+        btnSet.Size = New Size(130, 82)
+        btnSet.TabIndex = 6
+        btnSet.Text = "ESTABLECER"
+        btnSet.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(14, 19)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(99, 15)
+        Label1.TabIndex = 7
+        Label1.Text = "NOMBRE DE DNS"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(74, 51)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(39, 15)
+        Label2.TabIndex = 8
+        Label2.Text = "IPV4 1"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(74, 79)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(39, 15)
+        Label3.TabIndex = 9
+        Label3.Text = "IPV4 2"
+        ' 
+        ' Icono
+        ' 
+        Icono.ContextMenuStrip = ctMenu
+        Icono.Icon = CType(resources.GetObject("Icono.Icon"), Icon)
+        Icono.Text = "DNS Changer"
+        Icono.Visible = True
+        ' 
+        ' ctMenu
+        ' 
+        ctMenu.Items.AddRange(New ToolStripItem() {AbrirProgramaToolStripMenuItem, BorrarCachéToolStripMenuItem, CerrarToolStripMenuItem})
+        ctMenu.Name = "ctMenu"
+        ctMenu.Size = New Size(181, 92)
+        ' 
+        ' AbrirProgramaToolStripMenuItem
+        ' 
+        AbrirProgramaToolStripMenuItem.Name = "AbrirProgramaToolStripMenuItem"
+        AbrirProgramaToolStripMenuItem.Size = New Size(180, 22)
+        AbrirProgramaToolStripMenuItem.Text = "Abrir programa"
+        ' 
+        ' CerrarToolStripMenuItem
+        ' 
+        CerrarToolStripMenuItem.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point)
+        CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        CerrarToolStripMenuItem.Size = New Size(180, 22)
+        CerrarToolStripMenuItem.Text = "Cerrar"
+        ' 
+        ' BorrarCachéToolStripMenuItem
+        ' 
+        BorrarCachéToolStripMenuItem.Name = "BorrarCachéToolStripMenuItem"
+        BorrarCachéToolStripMenuItem.Size = New Size(180, 22)
+        BorrarCachéToolStripMenuItem.Text = "Borrar caché"
+        ' 
+        ' tmHide
+        ' 
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(512, 112)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btnSet)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnAdd)
+        Controls.Add(txtSecondaryDNS)
+        Controls.Add(txtPrimaryDNS)
+        Controls.Add(cmbDNSSets)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "DNS CHANGER"
+        ctMenu.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents cmbDNSSets As ComboBox
+    Friend WithEvents txtPrimaryDNS As TextBox
+    Friend WithEvents txtSecondaryDNS As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnSet As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Icono As NotifyIcon
+    Friend WithEvents tmHide As Timer
+    Friend WithEvents ctMenu As ContextMenuStrip
+    Friend WithEvents AbrirProgramaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BorrarCachéToolStripMenuItem As ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,218 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+Imports System.Net.NetworkInformation
+Imports System.Diagnostics
+Imports System.Net
+
+Public Class Form1
+    ' Estructura para almacenar cada conjunto de DNS
+    Public Class DNSSet
+        Public Property Name As String
+        Public Property PrimaryDNS As String
+        Public Property SecondaryDNS As String
+    End Class
+
+    ' Lista de conjuntos de DNS
+    Private dnsSets As New List(Of DNSSet)
+    Private dnsFilePath As String = "dns_sets.json"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "DNS CHANGER - " & Application.ProductVersion
+        LoadDNSData()
+        PopulateComboBox()
+        SelectCurrentSystemDNS()
+        Icono.BalloonTipText = cmbDNSSets.Text & " activo."
+        Icono.BalloonTipIcon = ToolTipIcon.Info
+        Icono.ShowBalloonTip(30000)
+        Icono.Visible = True
+        tmHide.Enabled = True
+    End Sub
+
+    ' Cargar datos de conjuntos de DNS desde el archivo JSON
+    Private Sub LoadDNSData()
+        If File.Exists(dnsFilePath) Then
+            Dim jsonData As String = File.ReadAllText(dnsFilePath)
+            dnsSets = JsonConvert.DeserializeObject(Of List(Of DNSSet))(jsonData)
+        End If
+    End Sub
+
+    ' Guardar conjuntos de DNS en el archivo JSON
+    Private Sub SaveDNSData()
+        Dim jsonData As String = JsonConvert.SerializeObject(dnsSets, Formatting.Indented)
+        File.WriteAllText(dnsFilePath, jsonData)
+    End Sub
+
+    ' Poblar ComboBox con nombres de conjuntos de DNS
+    Private Sub PopulateComboBox()
+        cmbDNSSets.Items.Clear()
+        For Each dnsSet In dnsSets
+            cmbDNSSets.Items.Add(dnsSet.Name)
+        Next
+    End Sub
+
+    ' Seleccionar el conjunto de DNS actual del sistema en el ComboBox
+    Private Sub SelectCurrentSystemDNS()
+        Dim currentDnsServers As List(Of String) = GetCurrentSystemDNSServers()
+
+        ' Buscar un conjunto en dnsSets que coincida con los DNS actuales (0 y 1 IPV6. 2 y 3 IPV4)
+        Dim matchingSet = dnsSets.FirstOrDefault(Function(d) d.PrimaryDNS = currentDnsServers(2) AndAlso
+                                                         d.SecondaryDNS = If(currentDnsServers.Count > 1, currentDnsServers(3), String.Empty))
+        If matchingSet IsNot Nothing Then
+            cmbDNSSets.SelectedItem = matchingSet.Name
+            txtPrimaryDNS.Text = matchingSet.PrimaryDNS
+            txtSecondaryDNS.Text = matchingSet.SecondaryDNS
+        End If
+    End Sub
+
+    ' Obtener los servidores DNS actuales del sistema
+    Private Function GetCurrentSystemDNSServers() As List(Of String)
+        Dim dnsServers As New List(Of String)
+        Dim networkConfig = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(Function(nic) nic.OperationalStatus = OperationalStatus.Up)
+
+        If networkConfig IsNot Nothing Then
+            Dim ipProps = networkConfig.GetIPProperties()
+            For Each dnsAddr In ipProps.DnsAddresses
+                dnsServers.Add(dnsAddr.ToString())
+            Next
+        End If
+
+        Return dnsServers
+    End Function
+
+    ' Agregar nuevo conjunto de DNS
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim newDNS As New DNSSet With {
+            .Name = InputBox("Nombre del conjunto de DNS:"),
+            .PrimaryDNS = txtPrimaryDNS.Text,
+            .SecondaryDNS = txtSecondaryDNS.Text
+        }
+
+        If Not String.IsNullOrEmpty(newDNS.Name) Then
+            dnsSets.Add(newDNS)
+            SaveDNSData()
+            PopulateComboBox()
+            cmbDNSSets.SelectedItem = newDNS.Name
+        End If
+    End Sub
+
+    ' Actualizar conjunto de DNS seleccionado
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        Dim selectedName As String = cmbDNSSets.SelectedItem?.ToString()
+        If Not String.IsNullOrEmpty(selectedName) Then
+            Dim dnsSet = dnsSets.FirstOrDefault(Function(d) d.Name = selectedName)
+            If dnsSet IsNot Nothing Then
+                dnsSet.PrimaryDNS = txtPrimaryDNS.Text
+                dnsSet.SecondaryDNS = txtSecondaryDNS.Text
+                SaveDNSData()
+            End If
+        End If
+    End Sub
+
+    ' Eliminar conjunto de DNS seleccionado
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        Dim selectedName As String = cmbDNSSets.SelectedItem?.ToString()
+        If Not String.IsNullOrEmpty(selectedName) Then
+            dnsSets.RemoveAll(Function(d) d.Name = selectedName)
+            SaveDNSData()
+            PopulateComboBox()
+            txtPrimaryDNS.Clear()
+            txtSecondaryDNS.Clear()
+        End If
+    End Sub
+
+    ' Cambiar el conjunto de DNS seleccionado en el ComboBox
+    Private Sub cmbDNSSets_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbDNSSets.SelectedIndexChanged
+        Dim selectedName As String = cmbDNSSets.SelectedItem?.ToString()
+        If Not String.IsNullOrEmpty(selectedName) Then
+            Dim dnsSet = dnsSets.FirstOrDefault(Function(d) d.Name = selectedName)
+            If dnsSet IsNot Nothing Then
+                txtPrimaryDNS.Text = dnsSet.PrimaryDNS
+                txtSecondaryDNS.Text = dnsSet.SecondaryDNS
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSet_Click(sender As Object, e As EventArgs) Handles btnSet.Click
+        Dim selectedDnsSet = dnsSets.FirstOrDefault(Function(d) d.Name = cmbDNSSets.SelectedItem?.ToString())
+        If selectedDnsSet IsNot Nothing Then
+            ' Establecer los DNS seleccionados
+            SetDNSServers(New List(Of String) From {selectedDnsSet.PrimaryDNS, selectedDnsSet.SecondaryDNS})
+            ' Flush DNS tras aplicar los cambios
+            FlushDNS()
+        End If
+    End Sub
+
+    ' Establecer los DNS seleccionados en el adaptador de red principal
+    Private Sub SetDNSServers(dnsServers As List(Of String))
+        Dim networkConfig = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(Function(nic) nic.OperationalStatus = OperationalStatus.Up)
+        If networkConfig IsNot Nothing Then
+            Dim processStartInfo As New ProcessStartInfo("netsh", $"interface ip set dns name=""{networkConfig.Name}"" source=static addr={dnsServers(0)}")
+            processStartInfo.Verb = "runas"
+            processStartInfo.CreateNoWindow = True
+            processStartInfo.UseShellExecute = False
+
+            Try
+                ' Aplicar el primer DNS
+                Process.Start(processStartInfo).WaitForExit()
+
+                ' Agregar DNS adicionales (si existen)
+                For i = 1 To dnsServers.Count - 1
+                    Dim additionalDNSInfo As New ProcessStartInfo("netsh", $"interface ip add dns name=""{networkConfig.Name}"" addr={dnsServers(i)} index={i + 1}")
+                    additionalDNSInfo.Verb = "runas"
+                    additionalDNSInfo.CreateNoWindow = True
+                    additionalDNSInfo.UseShellExecute = False
+                    Process.Start(additionalDNSInfo).WaitForExit()
+                Next
+                Icono.BalloonTipText = cmbDNSSets.Text & " activo."
+                Icono.BalloonTipIcon = ToolTipIcon.Info
+                Icono.ShowBalloonTip(30000)
+
+            Catch ex As Exception
+                MessageBox.Show($"Error al aplicar DNS: {ex.Message}")
+            End Try
+        End If
+    End Sub
+
+    ' Método para ejecutar flush DNS
+    Private Sub FlushDNS()
+        Dim processStartInfo As New ProcessStartInfo("ipconfig", "/flushdns")
+        processStartInfo.Verb = "runas"
+        processStartInfo.CreateNoWindow = True
+        processStartInfo.UseShellExecute = False
+
+        Try
+            Process.Start(processStartInfo).WaitForExit()
+            Icono.BalloonTipText = "Caché borrada"
+            Icono.BalloonTipIcon = ToolTipIcon.Info
+            Icono.ShowBalloonTip(30000)
+        Catch ex As Exception
+            MessageBox.Show($"Error al ejecutar flush DNS: {ex.Message}")
+        End Try
+    End Sub
+
+    Private Sub tmHide_Tick(sender As Object, e As EventArgs) Handles tmHide.Tick
+        Me.Hide()
+        tmHide.Enabled = False
+    End Sub
+
+    Private Sub AbrirProgramaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AbrirProgramaToolStripMenuItem.Click
+        Me.Show()
+    End Sub
+
+    Private Sub CerrarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CerrarToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        e.Cancel = True
+        Me.Hide()
+    End Sub
+
+    Private Sub Icono_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Icono.MouseDoubleClick
+        Me.Show()
+    End Sub
+
+    Private Sub BorrarCachéToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BorrarCachéToolStripMenuItem.Click
+        FlushDNS()
+    End Sub
+End Class
